bdadf38d6b9510ead166ba63332c73c9dbc71b16
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-04-27 10:45:35 +0200

    Add IEngine Interface into Nop.Core.Infrastructure namespace.

--- NopCommerceDemo/Nop.Core/Infrastructure/EngineContext.cs
+++ NopCommerceDemo/Nop.Core/Infrastructure/EngineContext.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Nop.Core.Infrastructure
 {
-    class EngineContext
+    /// <summary>
+    /// Provides access to the singlton instance of the Nop engine.
+    /// </summary>
+    public class EngineContext
     {
+
+        #region Methods
+
+        //// really is the same as using lock(this) on the whole method
+        //[MethodImpl(MethodImplOptions.Synchronized)]
+        //public static 
+
+        #endregion Methods
+
     }
 }
