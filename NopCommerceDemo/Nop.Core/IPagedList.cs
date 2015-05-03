bc44a64abaf5a4016f74231c60f72a66acac3573
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-03 14:36:20 +0200

    Add Nop.Services.Logging.DefaultLogger.cs class

--- NopCommerceDemo/Nop.Core/IPagedList.cs
+++ NopCommerceDemo/Nop.Core/IPagedList.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Nop.Core
 {
-    class IPagedList
+    /// <summary>
+    /// Paged list interface
+    /// </summary>
+    public interface IPagedList<T> : IList<T>
     {
+        int PageIndex { get; }
+        int PageSize { get; }
+        int TotalCount { get; }
+        int TotalPages { get; }
+        bool HasPreviousPage { get; }
+        bool HasNextPage { get; }
     }
 }
--- NopCommerceDemo/Nop.Services/Logging/ILogger.cs
+++ NopCommerceDemo/Nop.Services/Logging/ILogger.cs
@@ -1,4 +1,5 @@
-﻿using Nop.Core.Domain.Customers;
+﻿using Nop.Core;
+using Nop.Core.Domain.Customers;
 using Nop.Core.Domain.Logging;
 using System;
 using System.Collections.Generic;
@@ -32,7 +33,32 @@ namespace Nop.Services.Logging
         /// </summary>
         void ClearLog();
 
+        /// <summary>
+        /// Gets all log items
+        /// </summary>
+        /// <param name="fromUtc">Log item creation from; null to load all records</param>
+        /// <param name="toUtc">Log item creation to; null to load all records</param>
+        /// <param name="message">Message</param>
+        /// <param name="logLevel">Log level; null to load all records</param>
+        /// <param name="pageIndex">Page index</param>
+        /// <param name="pageSize">Page size</param>
+        /// <returns>Log item collection</returns>
+        IPagedList<Log> GetAllLogs(DateTime? fromUtc, DateTime? toUtc,
+            string message, LogLevel? logLevel, int pageIndex, int pageSize);
 
+        /// <summary>
+        /// Gets a log item
+        /// </summary>
+        /// <param name="logId">Log item identifier</param>
+        /// <returns>Log item</returns>
+        Log GetLogById(int logId);
+
+        /// <summary>
+        /// Get log items by identifiers
+        /// </summary>
+        /// <param name="logIds">Log item identifiers</param>
+        /// <returns>Lof items</returns>
+        IList<Log> GetLogByIds(int[] logIds);
 
         /// <summary>
         /// Insert a log item
