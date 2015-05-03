6b8f95dc1d51deef2e89fdd7ca9ca24006b63531
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-03 12:39:11 +0200

    Add Nop.Core.Infrastructure.ITypeFinder.cs interface

--- NopCommerceDemo/Nop.Core/Infrastructure/DependencyManagement/IDependencyRegistrar.cs
+++ NopCommerceDemo/Nop.Core/Infrastructure/DependencyManagement/IDependencyRegistrar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Nop.Core.Infrastructure.DependencyManagement
 {
-    interface IDependencyRegistrar
+    public interface IDependencyRegistrar
     {
+        //void Register(ContainerBuilder builder,ITypeFinder)
     }
 }
