efcb852131c353e9e19290f1f64bf9f5d564eb01
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-04 12:28:38 +0200

    Add Domain/Stores folder to Nop.Core project

--- NopCommerceDemo/Nop.Core/Domain/Localization/ILocalizedEntity.cs
+++ NopCommerceDemo/Nop.Core/Domain/Localization/ILocalizedEntity.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Nop.Core.Domain.Localization
 {
-    interface ILocalizedEntity
+    /// <summary>
+    /// Represents a localized entity
+    /// </summary>
+    public interface ILocalizedEntity
     {
     }
 }
