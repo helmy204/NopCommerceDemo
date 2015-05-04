0558615b10f1504dcce2474904e151d2477a877f
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-04 14:17:35 +0200

    Working in Nop.Core.WebHelper.cs class

--- NopCommerceDemo/Nop.Core/Domain/Stores/Store.cs
+++ NopCommerceDemo/Nop.Core/Domain/Stores/Store.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nop.Core.Domain.Localization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,59 @@ using System.Threading.Tasks;
 
 namespace Nop.Core.Domain.Stores
 {
-    class Store
+    /// <summary>
+    /// Represents a store
+    /// </summary>
+    public partial class Store : BaseEntity, ILocalizedEntity
     {
+        /// <summary>
+        /// Gets or sets the store name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the store URL
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether SSL is enabled
+        /// </summary>
+        public bool SslEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the store secure URL (HTTPS)
+        /// </summary>
+        public string SecureUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the comma separated list of possible HTTP_HOST values
+        /// </summary>
+        public string Hosts { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the company name
+        /// </summary>
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the company address
+        /// </summary>
+        public string CompanyAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the store phone number
+        /// </summary>
+        public string CompanyPhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the company VAT ( used in Europe Union countries )
+        /// </summary>
+        public string CompanyVat { get; set; }
     }
 }
--- NopCommerceDemo/Nop.Core/IStoreContext.cs
+++ NopCommerceDemo/Nop.Core/IStoreContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nop.Core.Domain.Stores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,9 @@ namespace Nop.Core
     /// </summary>
     public interface IStoreContext
     {
-
+        /// <summary>
+        /// Gets or sets the current store
+        /// </summary>
+        Store CurrentStore { get; }
     }
 }
