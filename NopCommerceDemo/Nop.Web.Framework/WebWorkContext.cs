484b352b4d12b832786b61153720889d25a43152
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-18 15:19:16 +0200

    Add System.Web reference to Nop.Web.Framework project

--- NopCommerceDemo/Nop.Web.Framework/WebWorkContext.cs
+++ NopCommerceDemo/Nop.Web.Framework/WebWorkContext.cs
@@ -1,4 +1,5 @@
 ﻿using Nop.Core;
+using Nop.Core.Domain.Customers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,54 @@ using System.Threading.Tasks;
 
 namespace Nop.Web.Framework
 {
+    // TODO: Not Finished
     /// <summary>
     /// Work context for web application
     /// </summary>
     public partial class WebWorkContext : IWorkContext
     {
-        public Core.Domain.Customers.Customer CurrentCustomer
+        #region Fields
+
+        //private readonly HttpContextBase
+
+        private Customer _cachedCustomer;
+
+        #endregion Fields
+
+        #region Properties
+
+        public Customer CurrentCustomer
+        {
+            get
+            {
+                if (_cachedCustomer != null)
+                    return _cachedCustomer;
+
+                Customer customer = null;
+                
+
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion Properties
+
+        public Core.Domain.Customers.Customer OriginalCustomerIfImpersonated
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+
+        public Core.Domain.Vendors.Vendor CurrentVendor
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Core.Domain.Localization.Language WorkingLanguage
         {
             get
             {
@@ -24,9 +67,40 @@ namespace Nop.Web.Framework
             }
         }
 
-        public Core.Domain.Customers.Customer OriginalCustomerIfImpersonated
+        public Core.Domain.Directory.Currency WorkingCurrency
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Core.Domain.Tax.TaxDisplayType TaxDisplayType
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsAdmin
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
