f598a332e200678291fc5fcf656d4aa35a2d9873
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-18 09:51:12 +0200

    Add Nop.Core.Domain.Seo.ISlugSupported.cs interface

--- NopCommerceDemo/Nop.Core/Domain/Vendors/Vendor.cs
+++ NopCommerceDemo/Nop.Core/Domain/Vendors/Vendor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nop.Core.Domain.Localization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Nop.Core.Domain.Vendors
 {
-    class Vendor
+    // TODO: Not Finished
+    public partial class Vendor : BaseEntity, ILocalizedEntity
     {
     }
 }
