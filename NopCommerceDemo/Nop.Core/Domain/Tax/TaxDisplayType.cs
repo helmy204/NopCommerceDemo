b53955b1f489768e720083ebd05a27831fb53ed3
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-18 14:00:19 +0200

    Finish Nop.Core.Domain.Tax.TaxDisplayType.cs enum

--- NopCommerceDemo/Nop.Core/Domain/Tax/TaxDisplayType.cs
+++ NopCommerceDemo/Nop.Core/Domain/Tax/TaxDisplayType.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Nop.Core.Domain.Tax
 {
-    class TaxDisplayType
+    /// <summary>
+    /// Represents the tax display type enumeration
+    /// </summary>
+    public enum TaxDisplayType
     {
+        /// <summary>
+        /// Including tax
+        /// </summary>
+        IncludingTax = 0,
+        /// <summary>
+        /// Excluding tax
+        /// </summary>
+        ExcludingTax = 10
     }
 }
