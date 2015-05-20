98563ddc1773e5a75f4638fac2414acd540f7d31
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-20 08:41:05 +0200

    Finished Nop.Services.Helpers.UserAgentHelper.cs class

--- NopCommerceDemo/Nop.Services/Helpers/UserAgentHelper.cs
+++ NopCommerceDemo/Nop.Services/Helpers/UserAgentHelper.cs
@@ -1,12 +1,84 @@
-﻿using System;
+﻿using Nop.Core;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using UserAgentStringLibrary;
 
 namespace Nop.Services.Helpers
 {
-    class UserAgentHelper
+    /// <summary>
+    /// User agent helper
+    /// </summary>
+    public partial class UserAgentHelper : IUserAgentHelper
     {
+        private readonly NopConfig _config;
+        private readonly IWebHelper _webHelper;
+        private readonly HttpContextBase _httpContext;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="config">Config</param>
+        /// <param name="webHelper">Web helper</param>
+        /// <param name="httpContext">HTTP context</param>
+        public UserAgentHelper(NopConfig config,IWebHelper webHelper,HttpContextBase httpContext)
+        {
+            this._config = config;
+            this._webHelper = webHelper;
+            this._httpContext = httpContext;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        protected virtual UasParser GetUasParser()
+        {
+            if (Singleton<UasParser>.Instance == null)
+            {
+                // no database created
+                if (String.IsNullOrEmpty(_config.UserAgentStringsPath))
+                    return null;
+
+                var filePath = _webHelper.MapPath(_config.UserAgentStringsPath);
+                var uasParser = new UasParser(filePath);
+                Singleton<UasParser>.Instance = uasParser;
+            }
+            return Singleton<UasParser>.Instance;
+        }
+
+        /// <summary>
+        /// Get a value indicating whether the request is made be search engine (web crawler)
+        /// </summary>
+        /// <returns>Result</returns>
+        public bool IsSearchEngine()
+        {
+            if (_httpContext == null)
+                return false;
+
+            // we put required logic in try-catch block
+            // more info:  http://www.nopcommerce.com/boards/t/17711/unhandled-exception-request-is-not-available-in-this-context.aspx
+            bool result = false;
+            try
+            {
+                var uasParser = GetUasParser();
+
+                // we cannot load parser
+                if (uasParser == null)
+                    return false;
+
+                var userAgent = _httpContext.Request.UserAgent;
+                result = uasParser.IsBot(userAgent);
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine(exc);
+            }
+            return result;
+        }
     }
 }
