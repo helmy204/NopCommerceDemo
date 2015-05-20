22048b32e29a69db300f3b4de50a06203cac63ce
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-20 09:46:23 +0200

    Add Nop.Services.Authentication.FormsAuthenticationService.cs class

--- NopCommerceDemo/Nop.Services/Authentication/IAuthenticationService.cs
+++ NopCommerceDemo/Nop.Services/Authentication/IAuthenticationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nop.Core.Domain.Customers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Nop.Services.Authentication
 {
-    interface IAuthenticationService
+    /// <summary>
+    /// Authentication service interface
+    /// </summary>
+    public partial interface IAuthenticationService
     {
+        void SignIn(Customer customer, bool createPersistentCookie);
+        void SignOut();
+        Customer GetAuthenticatedCustomer();
     }
 }
