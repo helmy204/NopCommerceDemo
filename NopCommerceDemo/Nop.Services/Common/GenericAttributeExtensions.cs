e51860d9d61baba98675eea151c358f212bf6d34
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-20 10:34:03 +0200

    Add Nop.Services.Common.IGenericAttributeService.cs interface

--- NopCommerceDemo/Nop.Services/Common/GenericAttributeExtensions.cs
+++ NopCommerceDemo/Nop.Services/Common/GenericAttributeExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nop.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Nop.Services.Common
 {
-    class GenericAttributeExtensions
+    public static class GenericAttributeExtensions
     {
+        //public static TPropType GetAttribute<TPropType>(this BaseEntity entity,
+        //    string key,IGe)
     }
 }
