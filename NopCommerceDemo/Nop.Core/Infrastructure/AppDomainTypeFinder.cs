a6a463f6457982c47d2d81d7d2df9a291022f2cd
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-24 09:11:41 +0200

    Add Nop.Core.Infrastructure.WebAppTypeFinder.cs class

--- NopCommerceDemo/Nop.Core/Infrastructure/AppDomainTypeFinder.cs
+++ NopCommerceDemo/Nop.Core/Infrastructure/AppDomainTypeFinder.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Nop.Core.Infrastructure
 {
-    class AppDomainTypeFinder
+    /// <summary>
+    /// A class that finds types needed by Nop by looping assemblies in the
+    /// currently executing AppDomain. Only assemblies whose names matches
+    /// certain patterns are investigated and an optional list of assemblies
+    /// referred by <see cref="AssemblyNames"/> are always investigated.
+    /// </summary>
+    public class AppDomainTypeFinder:ITypeFinder
     {
+        public IList<System.Reflection.Assembly> GetAssemblies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Type> FindClasswsOfType(Type assignTypeFrom, bool onlyConcreteClassess = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<System.Reflection.Assembly> assemblies, bool onlyConcreteClasses = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Type> FindClassesOfType<T>(bool onlyConcreteClasses = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Type> FindClassesOfType<T>(IEnumerable<System.Reflection.Assembly> assemblies, bool onlyConcreteClasses = true)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
