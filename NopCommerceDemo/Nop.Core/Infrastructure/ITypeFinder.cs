23e63b15074ca20dd224785dae324e493215c83d
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-24 09:08:18 +0200

    Add Nop.Core.Infrastructure.AppDomainTypeFinder.cs class

--- NopCommerceDemo/Nop.Core/Infrastructure/ITypeFinder.cs
+++ NopCommerceDemo/Nop.Core/Infrastructure/ITypeFinder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,15 @@ namespace Nop.Core.Infrastructure
     /// </summary>
     public interface ITypeFinder
     {
+        IList<Assembly> GetAssemblies();
+
+        // Concrete class is nothing but normal class.
+        IEnumerable<Type> FindClasswsOfType(Type assignTypeFrom, bool onlyConcreteClassess = true);
+
+        IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType<T>(bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
     }
 }
