adbac99703946ae12a0af5f08dc2c4651be7deb7
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-25 16:10:55 +0200

    Add Nop.Web.Framework.Mvc.NopModelBinder.cs class

--- NopCommerceDemo/Nop.Web.Framework/Mvc/NopModelBinder.cs
+++ NopCommerceDemo/Nop.Web.Framework/Mvc/NopModelBinder.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 
 namespace Nop.Web.Framework.Mvc
 {
-    /// <summary>
-    /// Base nopCommerce model
-    /// </summary>
-    //[ModelBinder(typeof(NopModelBinder]
-    class BaseNopModel
+    class NopModelBinder
     {
     }
 }
