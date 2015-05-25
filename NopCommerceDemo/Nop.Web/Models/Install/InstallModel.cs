847721a03c0aadde5418dfa59b61b1cd84176e17
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-25 15:33:45 +0200

    Add Nop.Web.Validators.Install.InstallValidator.cs class

--- NopCommerceDemo/Nop.Web/Models/Install/InstallModel.cs
+++ NopCommerceDemo/Nop.Web/Models/Install/InstallModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using FluentValidation.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Nop.Web.Models.Install
 {
+    //[Validator(typeof(InstallValidator))]
     public class InstallModel
     {
     }
