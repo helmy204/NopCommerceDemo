15420585d19b0e173842649c98934df7dc52eab4
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-25 15:40:50 +0200

    Add Nop.Web.Infrastructure.Installation.IInstallationLocalizationService.cs interface

--- NopCommerceDemo/Nop.Web.Framework/Validators/BaseNopValidator.cs
+++ NopCommerceDemo/Nop.Web.Framework/Validators/BaseNopValidator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace Nop.Web.Framework.Validators
 {
-    class BaseNopValidator
+    public abstract class BaseNopValidator<T>:AbstractValidator<T> where T : class
     {
+        protected BaseNopValidator()
+        {
+            PostInitialize();
+        }
+
+        /// <summary>
+        /// Developers can override this method in custom partial classes
+        /// in order to add some custom initialization code to constructors
+        /// </summary>
+        protected virtual void PostInitialize()
+        {
+
+        }
     }
 }
--- NopCommerceDemo/Nop.Web/Validators/Install/InstallValidator.cs
+++ NopCommerceDemo/Nop.Web/Validators/Install/InstallValidator.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Nop.Web.Framework.Validators;
+using Nop.Web.Models.Install;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Nop.Web.Validators.Install
 {
-    public class InstallValidator
+    public class InstallValidator : BaseNopValidator<InstallModel>
     {
+        //public InstallValidator()
     }
 }
