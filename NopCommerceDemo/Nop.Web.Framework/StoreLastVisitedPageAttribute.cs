bc2faaa425e053e9212eac6cfc953401f4432454
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-31 10:52:01 +0200

    Add Nop.Core.Domain.Customers.CustomerSettings.cs class

--- NopCommerceDemo/Nop.Web.Framework/StoreLastVisitedPageAttribute.cs
+++ NopCommerceDemo/Nop.Web.Framework/StoreLastVisitedPageAttribute.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Nop.Core.Data;
+using Nop.Core.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework
 {
-    class StoreLastVisitedPageAttribute
+    public class StoreLastVisitedPageAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+
+            if (filterContext == null || filterContext.HttpContext == null || filterContext.HttpContext.Request == null)
+                return;
+
+            //don't apply filter to child methods
+            if (filterContext.IsChildAction)
+                return;
+
+            //only GET requests
+            if (!String.Equals(filterContext.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+                return;
+
+            var customerSettings = EngineContext.Current.Resolve<CustomerSettings>();
+            if (!customerSettings.StoreLastVisitedPage)
+                return;
+
+            var webHelper = EngineContext.Current.Resolve<IWebHelper>();
+
+            base.OnActionExecuting(filterContext);
+        }
     }
 }
