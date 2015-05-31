37d28b5e32e346265587e8cf68e00705113774a7
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-05-31 12:37:38 +0200

    Add Nop.Services.Affiliates.IAffiliateService.cs interface

--- NopCommerceDemo/Nop.Web.Framework/CheckAffiliateAttribute.cs
+++ NopCommerceDemo/Nop.Web.Framework/CheckAffiliateAttribute.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Nop.Core.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework
 {
-    class CheckAffiliateAttribute
+    public class CheckAffiliateAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext == null || filterContext.HttpContext == null)
+                return;
+
+            HttpRequestBase request = filterContext.HttpContext.Request;
+            if (request == null)
+                return;
+
+            //don't apply filter to child methods
+            if (filterContext.IsChildAction)
+                return;
+
+            if(request.QueryString!=null&&request.QueryString["AffiliatedId"]!=null)
+            {
+                var affiliateId = Convert.ToInt32(request.QueryString["AffiliateId"]);
+
+                if (affiliateId > 0)
+                {
+                    //var affiliateService=EngineContext.Current.Resolve<IAffiliateService
+                }
+            }
+        }
     }
 }
