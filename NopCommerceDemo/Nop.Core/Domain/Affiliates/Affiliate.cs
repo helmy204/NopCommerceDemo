a392d551d3cb90e72179a8e110f477f6089093c5
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-06-01 09:25:42 +0200

    Add untracked files

---
+++ NopCommerceDemo/Nop.Core/Domain/Affiliates/Affiliate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Domain.Affiliates
+{
+    /// <summary>
+    /// Represents an affiliate
+    /// </summary>
+    public partial class Affiliate : BaseEntity
+    {
+        //-->>
+
+        //-->>
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity has been deleted
+        /// </summary>
+        public bool Deleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is active
+        /// </summary>
+        public bool Active { get; set; }
+
+        //-->>
+    }
+}
---
+++ NopCommerceDemo/Nop.Services/Events/IEventPublisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Services.Events
+{
+    /// <summary>
+    /// Event Publisher
+    /// </summary>
+    public interface IEventPublisher
+    {
+        /// <summary>
+        /// Publish event
+        /// </summary>
+        /// <typeparam name="T">T</typeparam>
+        /// <param name="eventMessage">Event Message</param>
+        void Publish<T>(T eventMessage);
+    }
+}
