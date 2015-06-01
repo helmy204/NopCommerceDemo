81b6199f3974fef2d31ef08bd0b9382655ebb2c2
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-06-01 10:24:47 +0200

    Add Nop.Services.SecurityIPermissionProvider.cs interface

--- NopCommerceDemo/Nop.Services/Security/IPermissionProvider.cs
+++ NopCommerceDemo/Nop.Services/Security/IPermissionProvider.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace Nop.Services.Security
 {
-    /// <summary>
-    /// Standard permission provider
-    /// </summary>
-    public partial class StandardPermissionProvider
+    interface IPermissionProvider
     {
     }
 }
