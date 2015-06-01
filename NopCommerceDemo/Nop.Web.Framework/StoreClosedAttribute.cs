74b4c76b7ed7ca2e8a7d577cccb5cf33e410442e
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-06-01 08:55:02 +0200

    Add Nop.Core.Domain.StoreInformationSettings.cs class

--- NopCommerceDemo/Nop.Web.Framework/StoreClosedAttribute.cs
+++ NopCommerceDemo/Nop.Web.Framework/StoreClosedAttribute.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Nop.Core.Data;
+using Nop.Core.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework
 {
-    class StoreClosedAttribute
+    public class StoreClosedAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext == null || filterContext.HttpContext == null)
+                return;
+
+            HttpRequestBase request = filterContext.HttpContext.Request;
+            if (request == null)
+                return;
+
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            if (String.IsNullOrEmpty(actionName))
+                return;
+
+            string controllerName = filterContext.Controller.ToString();
+            if (String.IsNullOrEmpty(controllerName))
+                return;
+
+            // don't apply filter to child methods
+            if (filterContext.IsChildAction)
+                return;
+
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+
+            //var storeInformationSettings=EngineContext.Current.Resolve
+        }
     }
 }
