db3063101fc95286c4a2cd2d3aa3f8ad0cc1e67b
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-06-01 09:25:17 +0200

    Add Nop.Services.Security.IPermissionService.cs interface

--- NopCommerceDemo/Nop.Web.Framework/PublicStoreAllowNavigationAttribute.cs
+++ NopCommerceDemo/Nop.Web.Framework/PublicStoreAllowNavigationAttribute.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Nop.Core.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework
 {
-    class PublicStoreAllowNavigationAttribute
+    public class PublicStoreAllowNavigationAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext == null || filterContext.HttpContext == null)
+                return;
+
+            HttpRequestBase request = filterContext.HttpContext.Request;
+            if (request == null)
+                return;
+
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            if (String.IsNullOrEmpty(actionName))
+                return;
+
+            string controllerName = filterContext.Controller.ToString();
+            if (String.IsNullOrEmpty(controllerName))
+                return;
+
+            //don't apply filter to child methods
+            if (filterContext.IsChildAction)
+                return;
+
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+
+            //var permissionService
+        }
     }
 }
