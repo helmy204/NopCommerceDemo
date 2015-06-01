0adb1e3a1a04e7eb37a2243ab678897d387d4637
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-06-01 13:21:03 +0200

    Add Nop.Core.Domain.Localization.LocalizationSettings.cs class

--- NopCommerceDemo/Nop.Web.Framework/LanguageSeoCodeAttribute.cs
+++ NopCommerceDemo/Nop.Web.Framework/LanguageSeoCodeAttribute.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Nop.Core.Data;
+using Nop.Core.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework
 {
-    class LanguageSeoCodeAttribute
+    /// <summary>
+    /// Attribute which ensures that store URL contains a language SEO code
+    /// if "SEO friendly URLs with multible languages" setting is enabled
+    /// </summary>
+    public class LanguageSeoCodeAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext == null || filterContext.HttpContext == null)
+                return;
+
+            HttpRequestBase request = filterContext.HttpContext.Request;
+            if (request == null)
+                return;
+
+            //don't apply filter to child methods
+            if (filterContext.IsChildAction)
+                return;
+
+            //only GET requests
+            if (!String.Equals(filterContext.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+                return;
+
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+
+            var localizationSettings=EngineContext.Current.Resolve<
+        }
     }
 }
