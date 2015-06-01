2ccd54d9335515515c641390fcd2894fe98c9cab
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-06-01 16:50:10 +0200

    Add Nop.Core.Domain.Seo.SeoSettings.cs class

--- NopCommerceDemo/Nop.Web.Framework/Seo/WwwRequirementAttribute.cs
+++ NopCommerceDemo/Nop.Web.Framework/Seo/WwwRequirementAttribute.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Nop.Core.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework.Seo
 {
-    class WwwRequirementAttribute
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class WwwRequirementAttribute : FilterAttribute, IAuthorizationFilter
     {
+        public virtual void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext == null)
+                throw new ArgumentNullException("filterContext");
+
+            //don't apply filter to child methods
+            if (filterContext.IsChildAction)
+                return;
+
+            // only redirect for GET requests, 
+            // otherwise the browser might not propagate the verb and request body correctly.
+            if (!String.Equals(filterContext.HttpContext.Request.HttpMethod, "GET", StringComparison.OrdinalIgnoreCase))
+                return;
+
+            //ignore this rule for localhost
+            if (filterContext.HttpContext.Request.IsLocal)
+                return;
+
+            if (!DataSettingsHelper.DatabaseIsInstalled())
+                return;
+        }
     }
 }
