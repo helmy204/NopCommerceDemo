03513f2c929c67cf4b9927a527ee443d92d8426f
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-06-01 14:15:21 +0200

    Add Nop.Web.Framework.Security.SslRequirement.cs enum

--- NopCommerceDemo/Nop.Web.Framework/Security/NopHttpsRequirementAttribute.cs
+++ NopCommerceDemo/Nop.Web.Framework/Security/NopHttpsRequirementAttribute.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Nop.Web.Framework.Security
 {
-    class NopHttpsRequirementAttribute
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class NopHttpsRequirementAttribute : FilterAttribute, IAuthorizationFilter
     {
+        public NopHttpsRequirementAttribute(Ssl)
+        {
+
+        }
+
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
